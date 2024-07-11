3ca32791039f33c70715b5efe80238e03b72f573
sS2S2bin <3dltnqls@naver.com>
2024-07-11 10:52:40 +0900

    ex03: 함수 컴포넌트

---
+++ 02.JSX Tutorials/ex03/src/Contents.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Contents(props) {
+    return (
+        <p>ex03 함수 컴포넌트</p>
+    );
+}
+
+export default Contents;
---
+++ 02.JSX Tutorials/ex03/src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from './Header';
+
+function Header(props) {
+    return (
+            <h1>Ex03</h1>
+    );
+}
+export default Header;
